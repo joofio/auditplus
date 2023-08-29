2de508cf7363d79e114eacf54dce515f30a375e0
João Almeida <joaofilipe90@gmail.com>
2023-08-29 19:00:43 +0100

    improve query

--- flaskapp/core/cache.py
+++ flaskapp/core/cache.py
@@ -12,23 +12,27 @@ def insert_into_cache(conn,nummecanografico,resultado,localpicagem,datahora):
         "datahora":datahora}
     )
     if resultado=="S":
-        sql = text("select * from auditcache.public.entry where nummecanografico= :nummecanografico and hora_saida is null")
-        result = conn.execute(sql, {"nummecanografico":nummecanografico}).fetchall()
+        sql = text("""select * from auditcache.public.entry where nummecanografico= :nummecanografico and hora_saida is null and local_picagem= :localpicagem and DATE_PART('day', :datahora ::timestamp - hora_entrada::timestamp) * 24 +
+              DATE_PART('hour', :datahora ::timestamp - hora_entrada::timestamp) between 1 and 24;""")
+        #print(sql)
+        result = conn.execute(sql, {"nummecanografico":nummecanografico,"localpicagem":localpicagem, "datahora":datahora}).fetchall()
         if len(result)==0:
-            return "error,saida sem entrada registada"
+            return "ERROR,saida sem entrada registada"
         elif len(result)>1:
             print(result)
             return "ERROR, more than one record"
         else:
-            sql = text("update auditcache.public.entry set hora_saida=:datahora where nummecanografico= :nummecanografico and hora_saida is null and local_picagem= :localpicagem")
+            print("updating")
+            sql = text("""update auditcache.public.entry set hora_saida=:datahora where nummecanografico= :nummecanografico and hora_saida is null and local_picagem= :localpicagem and DATE_PART('day', :datahora ::timestamp - hora_entrada::timestamp) * 24 +
+              DATE_PART('hour', :datahora ::timestamp - hora_entrada::timestamp) between 1 and 24""")
              
             conn.execute(sql,
-        {"datahora":datahora,"nummecanografico":nummecanografico,"localpicagem":localpicagem}
+        {"datahora":datahora,"nummecanografico":nummecanografico,"localpicagem":localpicagem,"datahora":datahora}
     )
     else:
         conn.execute(
         text(
-            "insert into auditcache.public.entry (nummecanografico,hora_entrada,local_picagem) values (:nummecanografico,:hora_entrada,:localpicagem)"
+            "insert into auditcache.public.entry (nummecanografico,hora_entrada,local_picagem) values (:nummecanografico,:hora_entrada,:localpicagem)  ON CONFLICT (nummecanografico,hora_entrada,local_picagem) DO NOTHING;"
         ),{
         "nummecanografico":nummecanografico,
         "hora_entrada":datahora,"localpicagem":localpicagem}
@@ -37,7 +41,9 @@ def insert_into_cache(conn,nummecanografico,resultado,localpicagem,datahora):
         
 
 def check_action(conn,nummecsonho,ADLOCPC_2,ADLOCPC_4,desunidade,dataaccao):
-    sql = text("select * from auditcache.public.entry where nummecanografico= :nummecsonho and hora_entrada <= :dataaccao and (hora_saida is null or hora_saida >= :dataaccao)")
+    sql = text("""select * from auditcache.public.entry where nummecanografico= :nummecsonho and hora_entrada <= :dataaccao and DATE_PART('day', :dataaccao ::timestamp - hora_entrada::timestamp) * 24 +
+              DATE_PART('hour', :dataaccao ::timestamp - hora_entrada::timestamp) between 0 and 24 and (DATE_PART('day', :dataaccao ::timestamp - hora_saida::timestamp) * 24 +
+              DATE_PART('hour', :dataaccao ::timestamp - hora_saida::timestamp) between -24 and 0 or hora_saida is null)""")
    # print(sql)
     result = conn.execute(sql, {"nummecsonho":nummecsonho,"dataaccao":dataaccao}).fetchall()
   #  print(result)
@@ -64,10 +70,11 @@ def check_action(conn,nummecsonho,ADLOCPC_2,ADLOCPC_4,desunidade,dataaccao):
 
 def check_match_picagem(localpicagem,ADLOCPC_4):
    # print(localpicagem,ADLOCPC_4)
-    dict_match={"AVEIRO":["CHBV Computadores"]}
-   # print(dict_match[localpicagem])
+    dict_match={"AVEIRO":["CHBV Computadores","Hospital Aveiro"]}
+    print(dict_match[localpicagem])
 
     if ADLOCPC_4 in dict_match[localpicagem]:
+        print("in here")
         return "ok"
     else:
         return "ERROR"
